6091520ceb8a1f5f5ca4a75747b5a706a089cf1b
miaomiaomiao-LJY <48284713+miaomiaomiao-LJY@users.noreply.github.com>
2020-07-07 15:38:52 +0800

    -- Make the initial page -- Tests click events -- Real machine operation test

--- app/src/main/java/com/example/homeaccesscontrolsystembasedonandroid/MainActivity.java
+++ app/src/main/java/com/example/homeaccesscontrolsystembasedonandroid/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.homeaccesscontrolsystembasedonandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    String TAG = "MainActivity";
+
+    private Button OpenDoorButton;
+    private Button ApplyAccessButton;
+    private Button ManageLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OpenDoorButton = findViewById(R.id.main_button_open_door);
+        OpenDoorButton.setOnClickListener(this);
+        ApplyAccessButton = findViewById(R.id.main_button_apply_access);
+        ApplyAccessButton.setOnClickListener(this);
+        ManageLoginButton = findViewById(R.id.main_button_manage_login);
+        ManageLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int select = v.getId();
+        switch (select) {
+            case R.id.main_button_open_door:
+                Log.i(TAG, "Clicked Button Open Door.");
+                break;
+            case R.id.main_button_apply_access:
+                Log.i(TAG, "Clicked Button Apply Access.");
+                break;
+            case  R.id.main_button_manage_login:
+                Log.i(TAG, "Clicked Button Manage Login.");
+                break;
+        }
     }
 }
